287db6d8f3fca662d66a931af06c9b707c473ad1
AJ Roper <ajr.frontend@gmail.com>
2025-04-19 17:54:42 -0400

    making search bar functional

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,11 +1,27 @@
 import "./searchBar.css";
+import React, { useState } from "react";
 
 export const SearchBar = () => {
+  const [input, setInput] = useState("");
+  const fetchData = (value) => {
+    fetch("https://dog.ceo/api/breeds/list/all").then((response) =>
+      response.json()
+    );
+    const results = json.filter;
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
   return (
     <div className="inputWrapper">
       <input
         className="searchBarInput"
         placeholder="Search your favorite breed..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
       />
     </div>
   );
